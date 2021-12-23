c61ff1f455ef928474d3713b4b7a7f7b30e63b6c
nishatoma <nishatoma@gmail.com>
2021-12-23 00:02:52 -0500

    change

--- src/SingleNumber136.java
+++ src/SingleNumber136.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SingleNumber136 {
+public class SingleNumber136 {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 1, 2, 3, 2, 4, 4};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+
+    }
 }
