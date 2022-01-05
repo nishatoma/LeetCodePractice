6c676f2f1281fae79b0b7ed7c281a90e683b6b77
nishatoma <nishatoma@gmail.com>
2022-01-04 21:01:20 -0500

    LC_41: First Missing Positive

---
+++ src/sorting/FirstMissingPositive_41.java
@@ -0,0 +1,26 @@
+package sorting;
+
+class FirstMissingPositive_41 {
+    
+    public int firstMissingPositive(int[] nums) {
+        int k = 0;
+        while (k < nums.length) {
+            int correctIndex = nums[k] - 1;
+
+            if (nums[k] > 0 && nums[k] < nums.length && nums[k] != nums[correctIndex]) {
+                int tmp = nums[k];
+                nums[k] = nums[correctIndex];
+                nums[correctIndex] = tmp;
+            } else {
+                k++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
