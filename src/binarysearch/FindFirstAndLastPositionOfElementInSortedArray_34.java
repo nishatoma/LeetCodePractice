5470e365eeb75bb9935db7c922a0072d1345be4c
nishatoma <nishatoma@gmail.com>
2022-01-09 23:33:33 -0500

    LC_34: Find First and Last Position of Element in Sorted Array update

--- src/binarysearch/FindFirstAndLastPositionOfElementInSortedArray_34.java
+++ src/binarysearch/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -7,46 +7,26 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
     }
 
     public int[] searchRange(int[] nums, int target) {
+        int first = search(nums, target, true);
+        int second = search(nums, target, false);
 
+        return new int[]{first, second};
+    }
 
+    private static int search(int[] nums, int target, boolean isLookingForStartIndex) {
         int left = 0;
         int right = nums.length - 1;
-        int firstFound = -1;
-        int secondFound = -1;
+        int answer = -1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                // potential answer found
-                if (mid > 0 && nums[mid] == nums[mid - 1]) {
-                    // search left!
+                answer = mid;
+                if (isLookingForStartIndex) {
                     right = mid - 1;
                 } else {
-                    firstFound = mid;
-                    break;
-                }
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        left = 0;
-        right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (nums[mid] == target) {
-                // potential answer found
-                if (mid < nums.length - 1 && nums[mid] == nums[mid + 1]) {
-                    // search right!
                     left = mid + 1;
-                } else {
-                    secondFound = mid;
-                    break;
                 }
             } else if (nums[mid] > target) {
                 right = mid - 1;
@@ -54,7 +34,6 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
                 left = mid + 1;
             }
         }
-
-        return new int[]{firstFound, secondFound};
+        return answer;
     }
 }
