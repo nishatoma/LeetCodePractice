422b7e2404ba4b04838dbd45d70994d265e053ef
Nisha Shlaymoon <nishatoma@gmail.com>
2022-03-30 22:52:01 -0400

    LC_3: Longest Substring Without Repeating Characters

---
+++ src/slidingwindow/LongestSubstringWithoutRepeatingCharacters3.java
@@ -0,0 +1,25 @@
+package slidingwindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters3 {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        int max = 0;
+        int l = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int r = 0; r < s.length(); ++r) {
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l));
+                l++;
+            }
+            set.add(s.charAt(r));
+            max = Math.max(max, r - l + 1);
+        }
+
+        return max;
+    }
+}
