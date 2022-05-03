c04f1f085581bcfbb900e5434f0d45b69b0e3ca1
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-03 17:20:43 -0400

    Update SwimInRisingWater_778.java

--- src/binarysearch/SwimInRisingWater_778.java
+++ src/binarysearch/SwimInRisingWater_778.java
@@ -2,13 +2,11 @@ class SwimInRisingWater_778 {
 
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        int lo = 0, hi = n*n - 1;
-        
-        boolean[][] visited = new boolean[n][n];
-        
+        int lo = grid[0][0], hi = n*n - 1;
+                    
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            
+            boolean[][] visited = new boolean[n][n];
             if (dfs(grid, 0, 0, mid, visited)) {
                 hi = mid;
             } else {
@@ -20,17 +18,21 @@ class SwimInRisingWater_778 {
     }
     
     static boolean dfs(int[][] grid, int r, int c, int e, boolean[][] visited) {
+        int n = grid.length;
         
-        if (r < 0 || r > grid.length - 1
-           || c < 0 || c > grid.length - 1
+        if (r < 0 || r >= n
+           || c < 0 || c >= n
            || visited[r][c]
            || grid[r][c] > e) return false;
         
         visited[r][c] = true;
         
-        return dfs(grid, r + 1, c, e, visited)
-            || dfs(grid, r, c + 1, e, visited)
-            || dfs(grid, r, c - 1, e, visited)
-            || dfs(grid, r - 1, c, e, visited);
+        if (r == n - 1 && c == n - 1) return true;
+        
+        
+        return dfs(grid, r - 1, c,     e, visited)
+            || dfs(grid, r + 1, c,     e, visited)
+            || dfs(grid, r    , c - 1, e, visited)
+            || dfs(grid, r    , c + 1, e, visited);
     }
 }
