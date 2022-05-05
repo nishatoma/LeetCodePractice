08b2c3a250195ead657d6af546dd56ed6e01dc28
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-05 17:00:40 -0400

    Create MaximumConfusionOfExam_2024.java

---
+++ src/binarysearch/MaximumConfusionOfExam_2024.java
@@ -0,0 +1,18 @@
+class MaximumConfusionOfExam_2024 {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        
+        int res = 0, 
+        maxf = 0, 
+        count[] = new int[128];
+        
+        for (int i = 0; i < answerKey.length(); ++i) {
+            maxf = Math.max(maxf, ++count[answerKey.charAt(i)]);
+            if (res - maxf < k)
+                res++;
+            else
+                count[answerKey.charAt(i - res)]--;
+        }
+        
+        return res;
+    }    
+}
