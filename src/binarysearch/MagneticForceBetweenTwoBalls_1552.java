e76dd33b323e745efb622ebaa052e9ac520bf007
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-08 00:58:36 -0400

    Create MagneticForceBetweenTwoBalls_1552.java

---
+++ src/binarysearch/MagneticForceBetweenTwoBalls_1552.java
@@ -0,0 +1,32 @@
+class MagneticForceBetweenTwoBalls_1552 {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int lo = 1, hi = position[position.length - 1];
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (isValid(position, mid, m)) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        
+        return lo - 1;
+    }
+    
+    private boolean isValid(int[] pos, int force, int m) {
+        int count = 1;
+        int lastPos = pos[0];
+        
+        for (int i = 1; i < pos.length; ++i) {
+            if (pos[i] - lastPos >= force) {
+                if (++count >= m) return true;
+                lastPos = pos[i];
+            }
+        }
+        
+        return false;
+    }
+}
