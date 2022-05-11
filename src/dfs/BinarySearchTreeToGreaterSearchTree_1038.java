cd3deec11c436f0b04bab79da4c950b0e4a2034c
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-11 09:13:36 -0400

    Create BinarySearchTreeToGreaterSearchTree_1038.java

---
+++ src/dfs/BinarySearchTreeToGreaterSearchTree_1038.java
@@ -0,0 +1,31 @@
+package dfs;
+
+import trees.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BinarySearchTreeToGreaterSearchTree_1038 {
+    
+    public TreeNode<Integer> bstToGst(TreeNode<Integer> root) {
+        Deque<TreeNode<Integer>> stack = new ArrayDeque<>();
+        TreeNode<Integer> curr = root;
+        int sum = 0;
+
+        while (!stack.isEmpty() || curr != null) {
+
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.getRight();
+            }
+            curr = stack.pop();
+
+            sum += curr.getVal();
+            curr.setVal(sum);
+            curr = curr.getLeft();
+
+        }
+
+        return root;
+    }
+}
