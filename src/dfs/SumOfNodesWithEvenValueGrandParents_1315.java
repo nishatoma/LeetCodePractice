8311a336b70881fc81201beb05e4744b83b798b3
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-10 21:56:37 -0400

    Create SumOfNodesWithEvenValueGrandParents_1315.java

---
+++ src/dfs/SumOfNodesWithEvenValueGrandParents_1315.java
@@ -0,0 +1,25 @@
+package dfs;
+
+import trees.TreeNode;
+
+public class SumOfNodesWithEvenValueGrandParents_1315 {
+
+    private int sum = 0;
+
+    public int sumEvenGrandparent(TreeNode root) {
+        dfs(root, null, null);
+
+        return sum;
+    }
+
+    private void dfs(TreeNode<Integer> curr, TreeNode<Integer> parent, TreeNode<Integer> grand) {
+        if (curr == null) return;
+
+        if (grand != null && (grand.getVal() & 1) == 0) {
+            sum += curr.getVal();
+        }
+
+        dfs(curr.getLeft(), curr, parent);
+        dfs(curr.getRight(), curr, parent);
+    }
+}
