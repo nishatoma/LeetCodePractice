f0e616ba798f6183498a03594d61266eda160beb
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-13 12:36:38 -0400

    Create NaryPreOrderTraversal_589.java

---
+++ src/dfs/NaryPreOrderTraversal_589.java
@@ -0,0 +1,31 @@
+package dfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaryPreOrderTraversal_589 {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) return res;
+
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node curr = stack.pop();
+
+            res.add(curr.val);
+
+            for (int i = curr.children.size() - 1; i >= 0; --i) {
+
+                stack.push(curr.children.get(i));
+            }
+
+        }
+
+        return res;
+    }
+}
