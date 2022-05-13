2c7a5da2abb04f387bc1f2e8f500bdf0d5e1da83
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-13 13:12:41 -0400

    Update NaryTreePostOrderTraversal_590.java

--- src/dfs/NaryTreePostOrderTraversal_590.java
+++ src/dfs/NaryTreePostOrderTraversal_590.java
@@ -4,25 +4,29 @@ import java.util.*;
 
 public class NaryTreePostOrderTraversal_590 {
 
+
 //    public List<Integer> postorder(Node root) {
 //        List<Integer> res = new ArrayList<>();
 //
 //        if (root == null) return res;
 //
 //        Deque<Node> stack = new ArrayDeque<>();
+//        Deque<Node> stack2 = new ArrayDeque<>();
 //        stack.push(root);
 //
 //        while (!stack.isEmpty()) {
 //            Node curr = stack.pop();
-//            res.add(curr.val);
+//            stack2.push(curr);
 //
 //            for (Node child: curr.children) stack.push(child);
 //
 //        }
 //
-//        Collections.reverse(res);
+//        while (!stack2.isEmpty()) {
+//            res.add(stack2.pop().val);
+//        }
 //
 //
 //        return res;
-//    }res
+//    }
 }
