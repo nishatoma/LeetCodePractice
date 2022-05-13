e346a904c49a737a6d854f8c7cef8e8075349790
Nisha Shlaymoon <nishatoma@gmail.com>
2022-05-13 13:04:18 -0400

    Update NaryPreOrderTraversal_589.java

--- src/dfs/NaryPreOrderTraversal_589.java
+++ src/dfs/NaryPreOrderTraversal_589.java
@@ -6,26 +6,26 @@ import java.util.List;
 
 public class NaryPreOrderTraversal_589 {
 
-    public List<Integer> preorder(Node root) {
-        List<Integer> res = new ArrayList<>();
-
-        if (root == null) return res;
-
-        Deque<Node> stack = new ArrayDeque<>();
-        stack.push(root);
-
-        while (!stack.isEmpty()) {
-            Node curr = stack.pop();
-
-            res.add(curr.val);
-
-            for (int i = curr.children.size() - 1; i >= 0; --i) {
-
-                stack.push(curr.children.get(i));
-            }
-
-        }
-
-        return res;
-    }
+//    public List<Integer> preorder(Node root) {
+//        List<Integer> res = new ArrayList<>();
+//
+//        if (root == null) return res;
+//
+//        Deque<Node> stack = new ArrayDeque<>();
+//        stack.push(root);
+//
+//        while (!stack.isEmpty()) {
+//            Node curr = stack.pop();
+//
+//            res.add(curr.val);
+//
+//            for (int i = curr.children.size() - 1; i >= 0; --i) {
+//
+//                stack.push(curr.children.get(i));
+//            }
+//
+//        }
+//
+//        return res;
+//    }
 }
