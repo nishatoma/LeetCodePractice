836a328e6722195a4a8f41bbb120931f0bfd083a
Nisha Shlaymoon <nishatoma@gmail.com>
2022-06-18 19:18:40 -0400

    Create TwoSumInputArrayIsSorted_167.java

---
+++ src/twopointers/TwoSumInputArrayIsSorted_167.java
@@ -0,0 +1,17 @@
+package twopointers;
+
+public class TwoSumInputArrayIsSorted_167 {
+    public int[] twoSum(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            if (nums[l] + nums[r] == target) return new int[]{l + 1, r + 1};
+            else if (nums[l] + nums[r] > target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return null;
+    }
+}
