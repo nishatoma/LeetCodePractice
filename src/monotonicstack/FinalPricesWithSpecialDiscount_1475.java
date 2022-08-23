2f86f0b2b4a00a3709050772b3e375bd00f2d41f
Nisha <nishatoma@gmail.com>
2022-08-23 16:59:13 -0400

    Create FinalPricesWithSpecialDiscount_1475.java

---
+++ src/monotonicstack/FinalPricesWithSpecialDiscount_1475.java
@@ -0,0 +1,17 @@
+package monotonicstack;
+
+import java.util.Stack;
+
+public class FinalPricesWithSpecialDiscount_1475 {
+
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prices.length; ++i) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                prices[stack.pop()] -= prices[i];
+            }
+            stack.push(i);
+        }
+        return prices;
+    }
+}
