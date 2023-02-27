8701afa8014e23a41b080d473cecdcb9fa94835f
Nisha Shlaymoon <nishatoma@gmail.com>
2023-02-27 12:26:31 -0500

    Create LC1268.java

---
+++ src/tries/LC1268.java
@@ -0,0 +1,80 @@
+package tries;
+
+import java.util.*;
+
+public class LC1268 {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Trie trie = new Trie();
+        // Sort the products array to maintain lexographical order
+        Arrays.sort(products);
+        // For each product, add it to Trie
+        for (String product : products) {
+            trie.insert(product);
+        }
+        // Make a list for result
+        List<List<String>> res = new ArrayList<>();
+        // Iterate over the root edges
+        TrieNode curr = trie.root;
+        for (char c : searchWord.toCharArray()) {
+            // get the suggestions list
+            if (curr != null) {
+                curr = curr.edges.get(c);
+            }
+            res.add(curr == null ? Arrays.asList() : curr.suggestions);
+        }
+        return res;
+    }
+
+    class TrieNode {
+        Map<Character, TrieNode> edges;
+        boolean isWord;
+        List<String> suggestions;
+
+
+        public TrieNode() {
+            this.edges = new HashMap<>();
+            this.isWord = false;
+            suggestions = new ArrayList<>();
+        }
+    }
+
+    class Trie {
+
+        static final int MAX = 3;
+        TrieNode root;
+
+
+        public Trie() {
+            this.root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode curr = this.root;
+            for (char c : word.toCharArray()) {
+                if (!curr.edges.containsKey(c)) {
+                    // Then add
+                    curr.edges.put(c, new TrieNode());
+                }
+                // Update current
+                curr = curr.edges.get(c);
+                // Also add to suggestions
+                if (curr.suggestions.size() < MAX) {
+                    curr.suggestions.add(word);
+                }
+            }
+            // In the end, this trie node is a word
+            curr.isWord = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode curr = this.root;
+            for (char c : word.toCharArray()) {
+                if (!curr.edges.containsKey(c)) {
+                    return false;
+                }
+                curr = curr.edges.get(c);
+            }
+            return curr.isWord;
+        }
+    }
+}
