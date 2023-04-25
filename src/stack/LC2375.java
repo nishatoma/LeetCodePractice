239b4dac8e7878ed4a5119d516bb77613a5d1d60
Nisha Shlaymoon <nishatoma@gmail.com>
2023-04-25 14:20:12 -0400

    Create LC2375.java

---
+++ src/stack/LC2375.java
@@ -0,0 +1,15 @@
+package stack;
+
+public class LC2375 {
+    public String smallestNumber(String pattern) {
+        StringBuilder res = new StringBuilder(), stack = new StringBuilder();
+        for (int i = 0; i <= pattern.length(); ++i) {
+            stack.append((char) ('1' + i));
+            if (i == pattern.length() || pattern.charAt(i) == 'I') {
+                res.append(stack.reverse());
+                stack = new StringBuilder();
+            }
+        }
+        return res.toString();
+    }
+}
