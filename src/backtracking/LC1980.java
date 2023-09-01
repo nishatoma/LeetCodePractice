81c6772845e8a6496f7b7159c787776750338c80
Nisha Shlaymoon <nishatoma@gmail.com>
2023-09-01 17:41:36 -0400

    Create LC1980.java

---
+++ src/backtracking/LC1980.java
@@ -0,0 +1,11 @@
+package backtracking;
+
+public class LC1980 {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; ++i) {
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        }
+        return sb.toString();
+    }
+}
